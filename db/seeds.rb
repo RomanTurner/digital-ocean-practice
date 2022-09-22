eaadcf3113f98efec781855d3dcc1ff2f07e4fa4
roman turner <roman.nturner@gmail.com>
2022-09-21 19:26:31 -0700

    init commit

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+5.times do |n|
+  Post.create(title: "The boy who lived #{n}", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quidem veritatis labore? Quasi fuga asperiores, eveniet debitis quaerat facere nihil totam. Temporibus, obcaecati assumenda at quia pariatur ut ipsum perferendis.")
+end
